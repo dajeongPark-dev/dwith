2ba13898a7e0364e298bb692577cc6eba3e2dd26
dajeongPark-dev <jul897813@gmail.com>
2021-08-11 12:28:10 +0900

    Update App.vue
    add router-link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/bulletinBoard">bulletinBoard</router-link> |
+      <router-link to="/chatting">chatting</router-link> |
+      <router-link to="/information">information</router-link> |
+      <router-link to="/myPage">myPage</router-link>
     </div>
     <router-view />
   </div>
--- src/views/Chatting.vue
+++ src/views/Chatting.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="chatting">
-     <h1>This is a chatting page</h1>
+    <h1>This is a chatting page</h1>
   </div>
 </template>
