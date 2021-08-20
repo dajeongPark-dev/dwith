3a626e20bab96dd7d32a4263973d4fa3b64ff369
dajeongPark-dev <jul897813@gmail.com>
2021-08-20 16:13:51 +0900

    Create SwitchButton.vue
    Change the type of the bulletin board

---
+++ src/components/board/SwitchButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="switchButton" class="post-show">
+    <router-link to="/"
+      ><b-button
+        variant="light"
+        @click="gotoPage(false)"
+        :class="[isActive === 0 ? 'button-selected' : 'post-box-default']"
+      >
+        {{ bbsName }}
+      </b-button></router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //현재 페이지 번호
+      isActive: false,
+      bbsName: "일반형",
+    };
+  },
+  methods: {
+    //페이지(라우터)클릭시 페이지 번호 변경
+    gotoPage(state) {
+      this.isActive = state;
+    },
+  },
+};
+</script>
